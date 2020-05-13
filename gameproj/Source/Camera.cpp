a267fd1478d439e9e2ec4ad4ea0e80d77259fc13
JohnSmoit <ju.smoit@gmail.com>
2020-05-13 14:36:44 -0700

    Begin Camera Creation

--- gameproj/Source/Camera.cpp
+++ gameproj/Source/Camera.cpp
@@ -1,14 +1,38 @@
-// #include "Camera.hpp"
+#include "Camera.hpp"
+#include "PhysicsManager.hpp"
 
-// Camera::Camera(GameObject* obj, SDL_Rect v) 
-// {
-//     view = v;
-//     anchor = obj;
-//     position = anchor->getPosition();
-// }
+Camera::Camera(GameObject* obj, SDL_Rect v) 
+{
+    view = v;
+    anchor = obj;
+    position = *anchor->getPosition();
+}
 
-// void Camera::follow() 
-// {
-//     position =  anchor->getPosition();
+void Camera::follow() 
+{
+    Vector2* pos = anchor->getPosition();
+    pos->x -= view.w / 2;
+    pos->y -= view.h / 2;
+    position = *pos;
     
-// }
+}
+
+void Camera::GetObjectsForRendering(std::vector<GameObject*> objList) 
+{
+    std::vector<GameObject*> renderList;
+    for (int i = 0; i < objList.size(); i++) 
+    {
+        if (PhysicsManager::checkAABBCollisions(this, objList[i])) 
+        {
+            renderList.push_back(objList[i]);
+        }
+    }
+}
+
+void Camera::RenderObjects(std::vector<GameObject*> list) 
+{
+    for (int i = 0; i < list.size(); i++) 
+    {
+        float offsetX, offsetY;
+    }
+}
